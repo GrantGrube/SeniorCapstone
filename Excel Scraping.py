9b5a55a8ae8746ad10444d7d32e85d709c43d09f
GrantGrube <pyrogamez123@gmail.com>
2026-02-24 10:07:01 -0500

    Add files via upload

--- Excel Scraping.py
+++ Excel Scraping.py
@@ -7,15 +7,16 @@ book = load_workbook(filename="Capstone Sheet.xlsx") #loading the sheet we want 
 
 print(book.sheetnames)
 sheetn = input("Please enter the name of the sheet you would like to use: ")
-current = book[sheetn]
+current = book[sheetn] #change current sheet to the one selected
 print("you have chosen " + current.title)
 
 
-cell_data = []
+cell_data = [] #initialize the array for each cell
+
 
 for row in current.iter_rows():
     for cell in row:
-        cell_data.append({
+        cell_data.append({     #adding specific data from each cell
             "row": cell.row,
             "col": cell.column,
             "coordinate": cell.coordinate,
@@ -23,10 +24,10 @@ for row in current.iter_rows():
             "type": type(cell.value).__name__
         })
 
+#exporting directly to Unity Asset file 
 output_path = r"\Users\Grant\Desktop\Capstone\UnityCapstone\Assets\StreamingAssets/excel_data.json"
-
-
-
 with open(output_path, "w") as f:
     json.dump(cell_data, f, indent=4)
+
+print("JSON formatting complete, please check Asset file in Unity")
 
