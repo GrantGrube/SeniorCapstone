e90437253676e6dc524891c63e5b40101025dc9b
GrantGrube <pyrogamez123@gmail.com>
2026-04-12 17:27:02 -0400

    Unity, Excel Scraping, and ELO Scraping
    4/12 Version: Excel scraping is finished and working. ELO scraping not finished, needs other statistics. Unity still needs multiple sheets, fixed rotation of text, and cell blocking text to be added/fixed.

---
+++ ELOScraping.py
@@ -0,0 +1,26 @@
+import requests
+import csv
+from io import StringIO
+
+url = "https://www.eloratings.net/World.tsv?_=1774309604123"
+response = requests.get(url)
+
+tsv_data = StringIO(response.text)
+reader = csv.reader(tsv_data, delimiter="\t")
+
+
+with open("ratings.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    for row in reader:
+        rating = row[1]
+        team = row[2]
+        ELO = row[3]
+        Total = row[25]
+        print(rating, team, ELO, Total)
+        writer.writerow([rating, team, ELO])
+
+#.split \t function to split data into rows to scrape
+
+
+    
+print("Saved to ratings.csv")
